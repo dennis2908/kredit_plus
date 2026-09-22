6102f42d957cfe76aa0f207211ce0829c659948f
dennis2908 <manullang_d@yahoo.com>
2026-09-22 08:27:01 +0700

    add postman

--- main.go
+++ main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	_ "fmt"
-<<<<<<< HEAD
-=======
-	Connection "kredit_plus/connects"
-
-	Elastic "kredit_plus/elastic"
->>>>>>> 8fc39fceb076d2ab4ee9fb94e2b9db00d2abf9fe
 	_ "kredit_plus/routers"
 	"kredit_plus/ssrf"
-	"kredit_plus/token"
+	"log"
 	"os"
 	"runtime"
-	"sync"
-
-	"log"
-	"net/http"
 	"strconv"
 
 	"github.com/astaxie/beego"
@@ -25,63 +15,10 @@ import (
 	"github.com/beego/beego/v2/client/cache"
 	"github.com/beego/beego/v2/core/logs"
 	_ "github.com/lib/pq"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type responseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-var (
-	codeCounter = prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Name: "http_request_total_code",
-			Help: "total request code controller",
-		},
-	)
-)
-
-func NewResponseWriter(w http.ResponseWriter) *responseWriter {
-	return &responseWriter{w, http.StatusOK}
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
-}
-
-var totalRequests = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "http_requests_total",
-		Help: "Number of get requests.",
-	},
-	[]string{"path"},
-)
-
-var responseStatus = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "response_status",
-		Help: "Status of HTTP response",
-	},
-	[]string{"status"},
-)
-
-var httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-	Name: "http_response_time_seconds",
-	Help: "Duration of HTTP requests.",
-}, []string{"path"})
-
-func init() { // init instead of int
+func init() {
 	beego.Debug("Filters init...")
-
-<<<<<<< HEAD
-	// CORS for https://foo.* origins, allowing:
-	// - PUT and PATCH methods
-	// - Origin header
-	// - Credentials share
 
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowOrigins:     []string{"*"},
@@ -90,66 +27,52 @@ func init() { // init instead of int
 		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
 		AllowCredentials: true,
 	}))
+
 	orm.RegisterDriver("postgres", orm.DRPostgres)
-	orm.RegisterDataBase("default",
+	
+	// Updated host from netwkreditPlus to localhost
+	err := orm.RegisterDataBase("default",
 		"postgres",
-		"user=postgres password=123456 host=127.0.0.1 port=5432 dbname=kredit_plus sslmode=disable")
+		"user=postgres password=123456 host=localhost port=5432 dbname=kredit_plus sslmode=disable")
+	if err != nil {
+		log.Fatalf("Failed to register database: %v", err)
+	}
+
 	orm.RunSyncdb("default", false, true)
 	orm.RunCommand()
-=======
-	Connection.Connects()
-	Elastic.Connect()
-	prometheus.Register(totalRequests)
-	prometheus.Register(responseStatus)
-	prometheus.Register(httpDuration)
-	prometheus.MustRegister(codeCounter)
->>>>>>> 8fc39fceb076d2ab4ee9fb94e2b9db00d2abf9fe
 }
+
 func main() {
 	err := ssrf.Main()
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
+	// Optimize Go runtime thread allocation
 	numberOfCores := runtime.NumCPU()
 	runtime.GOMAXPROCS(numberOfCores)
-	var wg sync.WaitGroup
-	for i := 0; i < numberOfCores; i++ {
-		wg.Add(1)
-		_, err := cache.NewCache("file", `{"CachePath":"./cache","FileSuffix":".cache", "EmbedExpiry": "120"}`)
 
-		beego.InsertFilter("/konsumen/*", beego.BeforeExec, token.ValidateToken)
-		beego.InsertFilter("/transaksi/*", beego.BeforeExec, token.ValidateToken)
-		beego.InsertFilter("/transaksidetails/*", beego.BeforeExec, token.ValidateToken)
-		beego.InsertFilter("/konsumen_mongo_insert/*", beego.BeforeExec, token.ValidateToken)
-		beego.InsertFilter("/konsumen_mongo_update/*", beego.BeforeExec, token.ValidateToken)
-		beego.InsertFilter("/konsumen_excel_read/*", beego.BeforeExec, token.ValidateToken)
-
-		// Prometheus endpoint
-		beego.Handler("/metrics", promhttp.Handler())
-
-		orm.Debug = true
-
-		o := orm.NewOrm()
-		o.Using("default")
-
-		if err != nil {
-			logs.Error(err)
-		}
-		log.Println("Env $PORT :", os.Getenv("PORT"))
-		if os.Getenv("PORT") != "" {
-			port, err := strconv.Atoi(os.Getenv("PORT"))
-			if err != nil {
-				log.Fatal(err)
-				log.Fatal("$PORT must be set")
-			}
-			log.Println("port : ", port)
-			beego.BConfig.Listen.HTTPPort = port
-			beego.BConfig.Listen.HTTPSPort = port
-		}
-
-		beego.Run()
+	// Initialize Cache
+	_, err = cache.NewCache("file", `{"CachePath":"./cache","FileSuffix":".cache", "EmbedExpiry": "120"}`)
+	if err != nil {
+		logs.Error(err)
 	}
-	wg.Wait()
 
+	orm.Debug = true
+
+	// Handle Port configuration
+	log.Println("Env $PORT :", os.Getenv("PORT"))
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		port, err := strconv.Atoi(envPort)
+		if err != nil {
+			log.Fatalf("Invalid $PORT value: %v", err)
+		}
+		log.Println("port : ", port)
+		beego.BConfig.Listen.HTTPPort = port
+		beego.BConfig.Listen.HTTPSPort = port
+	}
+
+	// Start web server (Blocking call)
+	beego.Run()
 }
